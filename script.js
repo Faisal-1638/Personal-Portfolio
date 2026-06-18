7dd91df00a7d9e9bc88716742c62766f01589a1b
Faisal-1638 <fahmed8573@gmail.com>
2026-06-18 22:54:48 +0600

    removed javascript code for single pages

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function () {
-  const btn = document.querySelector('.ftop');
-
-  if (btn) {
-    btn.style.display = window.scrollY > 200 ? 'flex' : 'none';
-  }
-});
