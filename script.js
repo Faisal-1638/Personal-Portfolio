f22135ced9a0ecdc09e230da97c535a35b4deb68
Faisal-1638 <fahmed8573@gmail.com>
2026-07-02 19:58:46 +0600

    bug fixed

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-const form = document.getElementById("contactForm");
-
-form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    const data = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
-    };
-
-    try {
-        const response = await fetch("https://script.google.com/macros/s/AKfycbwk4lbL3Z_LJ7BDwtJZ4eh9wbQM6v6x6cEhwwCh1Nt_n-6NkkaZy0dkCr9gj6tkFEVuyA/exec", 
-            {
-            method: "POST", mode: "no-cors",
-            headers: {
-                "Content-Type": "application/json"
-            },
-
-            body: JSON.stringify(data)
-        });
-
-        alert("✅ Message sent successfully!");
-
-        form.reset();
-
-    } catch (error) {
-        alert("❌ Failed to send message.");
-        console.error(error);
-    }
+const response = await fetch("https://script.google.com/macros/s/AKfycbwi63Gb8Crr6krteo-Kt4yQE31A5XFt4zdUj_VAFeOwdZe-LCOeCNM0X6G5qCvle2B_gg/exec", {
+    method: "POST",
+    headers: {
+        "Content-Type": "text/plain;charset=utf-8" 
+    },
+    body: JSON.stringify(data)
 });
+
+const result = await response.json();
+
+if (result.result === "success") {
+    alert("✅ Message sent successfully!");
+    form.reset();
+} else {
+    alert("❌ Failed to send message.");
+    console.error(result.error);
+}
